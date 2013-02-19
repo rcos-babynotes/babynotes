c43331d6fd7510b41c288fc311559d52467a8598
Damian Mastylo <dmastylo@codequarry.net>
2013-02-19 15:25:40 -0500

    Views and basic setup for babies. Actual forms need to be setup

--- app/controllers/babies_controller.rb
+++ app/controllers/babies_controller.rb
@@ -1,2 +1,28 @@
 class BabiesController < ApplicationController
+  before_filter :user_owns_baby, only: [:show]
+
+  def index
+    @babies = current_user.babies
+  end
+
+  def show
+
+  end
+
+  def new
+    
+  end
+
+  def create
+
+  end
+
+private
+  def user_owns_baby
+    @baby = current_user.babies.find(params[:id])
+
+    if @baby.nil?
+      redirect_to root_path
+    end
+  end
 end
