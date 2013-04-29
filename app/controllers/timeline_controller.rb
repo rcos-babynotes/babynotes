b2331092fdee6c82b623a6ffa356c982b8e377ff
Damian Mastylo <dmastylo@codequarry.net>
2013-04-29 19:13:37 -0400

    Made the data structure easier to work with

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 class TimelineController < ApplicationController
   before_filter :authenticate_user!
 
@@ -5,6 +7,7 @@ class TimelineController < ApplicationController
     @baby = current_user.babies.first
     @events = @baby.events.order('happened_at DESC').group_by { |event| event.happened_at.to_date.beginning_of_day }
     @measurements = @baby.measurements.order('happened_at DESC').group_by { |measurement| measurement.happened_at.to_date.beginning_of_day }
-    @days = @events.merge(@measurements) { |key, oldval, newval| [*oldval].to_a + [*newval].to_a }
+    @days = @events.merge(@measurements) { |key, events, measurements| { 'events' => events, 'measurements' => measurements } }
+    # 
   end
 end
