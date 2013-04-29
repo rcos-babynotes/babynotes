6e37ea564d87f57eb605c047d4fc5cabb236824d
Damian Mastylo <dmastylo@codequarry.net>
2013-04-29 18:36:32 -0400

    More timeline changes

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -5,5 +5,6 @@ class TimelineController < ApplicationController
     @baby = current_user.babies.first
     @events = @baby.events.order('happened_at DESC').group_by { |event| event.happened_at.to_date.beginning_of_day }
     @measurements = @baby.measurements.order('happened_at DESC').group_by { |measurement| measurement.happened_at.to_date.beginning_of_day }
+    @days = @events.merge(@measurements) { |key, oldval, newval| [*oldval].to_a + [*newval].to_a }
   end
 end
