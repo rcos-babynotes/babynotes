a2fa0759890d8e24e557bd6ea08c28919bcc0bc7
Damian Mastylo <dmastylo@codequarry.net>
2013-04-29 16:44:44 -0400

    filtering/grouping by day

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,7 +2,8 @@ class TimelineController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @events = current_user.babies.first.events.order('happened_at DESC').group_by { |event| event.created_at.to_date.beginning_of_day }
-    @measurements = current_user.babies.first.measurements.order('created_at DESC').group_by { |measurement| measurement.created_at.to_date.beginning_of_day }
+    @baby = current_user.babies.first
+    @events = @baby.events.order('happened_at DESC').group_by { |event| event.happened_at.to_date.beginning_of_day }.to_a
+    @measurements = @baby.measurements.order('created_at DESC').group_by { |measurement| measurement.created_at.to_date.beginning_of_day }.to_a
   end
 end
