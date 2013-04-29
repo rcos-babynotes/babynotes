faab6c5116cc6f716b6b0a45317685b048ab1584
Damian Mastylo <dmastylo@codequarry.net>
2013-04-29 17:07:35 -0400

    More work on the days

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -3,7 +3,7 @@ class TimelineController < ApplicationController
 
   def index
     @baby = current_user.babies.first
-    @events = @baby.events.order('happened_at DESC').group_by { |event| event.happened_at.to_date.beginning_of_day }.to_a
-    @measurements = @baby.measurements.order('created_at DESC').group_by { |measurement| measurement.created_at.to_date.beginning_of_day }.to_a
+    @events = @baby.events.order('happened_at DESC').group_by { |event| event.happened_at.to_date.beginning_of_day }
+    @measurements = @baby.measurements.order('happened_at DESC').group_by { |measurement| measurement.happened_at.to_date.beginning_of_day }
   end
 end
